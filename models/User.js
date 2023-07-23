b727827b2222cb71377e099b4072592c7659a921
retnodamayanti <mayahere5@gmail.com>
2023-07-23 17:37:59 +0930

    added virtual true

--- models/User.js
+++ models/User.js
@@ -1,31 +1,36 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: /^[\w-]+@[a-zA-Z\d]+(\.[a-zA-Z\d]+)*(\.[a-zA-Z]{2,})$/,
-  },
-  thoughts: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Thought', 
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
-  ],
-  friends: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User', 
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^[\w-]+@[a-zA-Z\d]+(\.[a-zA-Z\d]+)*(\.[a-zA-Z]{2,})$/,
     },
-  ],
-});
+    thoughts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: { virtuals: true }, 
+  }
+);
 
 userSchema.virtual('friendCount').get(function () {
   return this.friends.length;

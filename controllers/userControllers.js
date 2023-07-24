945d5389c3543f4c8619b2cb32f7c49dd8239043
retnodamayanti <mayahere5@gmail.com>
2023-07-24 14:04:43 +0930

    updated get all user

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,20 +2,8 @@ const User = require('../models/User');
 
 // Controller functions for user routes
 exports.getAllUsers = async (req, res) => {
-  try {
-    const users = await User.find().populate('thoughts friends');
-    res.json(users);
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to get users.' });
-  }
-};
-
-exports.getUserById = async (req, res) => {
     try {
-      const userId = req.params.id;
-      
-      // Find the user by its ID and populate only the '_id' values of 'thoughts' and 'friends'
-      const user = await User.findById(userId)
+      const users = await User.find()
         .populate({
           path: 'thoughts',
           select: '_id',
@@ -25,21 +13,63 @@ exports.getUserById = async (req, res) => {
           select: '_id',
         });
   
+      // Iterate through the users and remove unnecessary data from the thoughts and friends arrays
+      const usersWithIdsOnly = users.map((user) => ({
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        thoughts: user.thoughts.map((thought) => thought._id),
+        friends: user.friends.map((friend) => friend._id),
+        friendCount: user.friends.length,
+      }));
+  
+      res.json(usersWithIdsOnly);
+    } catch (err) {
+      res.status(500).json({ error: 'Failed to get users.' });
+    }
+  };
+  
+
+  exports.getUserById = async (req, res) => {
+    try {
+      const userId = req.params.id;
+  
+      // Find the user by its ID and populate both the '_id' and 'reactions' fields of 'thoughts'
+      const user = await User.findById(userId)
+        .populate({
+          path: 'thoughts',
+          select: '_id reactions', // Populate both '_id' and 'reactions' fields
+        })
+        .populate('friends');
+  
       if (!user) {
         return res.status(404).json({ message: 'User not found.' });
       }
   
-      // Include the 'friendCount' property in the response
+      // Ensure the 'friends' field is set to an empty array if it doesn't exist
+      user.friends = user.friends || [];
+  
+      // Create a new object with the necessary properties
       const userWithFriendCount = {
-        ...user._doc,
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        thoughts: user.thoughts,
+        friends: user.friends,
         friendCount: user.friends.length,
       };
   
       res.json(userWithFriendCount);
     } catch (err) {
+      // Log the error for debugging purposes
+      console.error(err);
       res.status(500).json({ error: 'Failed to get the user.' });
     }
   };
+  
+  
+  
+  
   
 
 exports.createUser = async (req, res) => {
@@ -69,16 +99,19 @@ exports.updateUser = async (req, res) => {
 };
 
 exports.deleteUser = async (req, res) => {
-  try {
-    const deletedUser = await User.findByIdAndRemove(req.params.id);
-    if (!deletedUser) {
-      return res.status(404).json({ message: 'User not found.' });
+    try {
+      const deletedUser = await User.findByIdAndDelete(req.params.id);
+      if (!deletedUser) {
+        return res.status(404).json({ message: 'User not found.' });
+      }
+  
+      res.json({ message: 'User has been deleted.' });
+    } catch (err) {
+        console.error(err);
+      res.status(500).json({ error: 'Failed to delete the user.' });
     }
-    res.json(deletedUser);
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to delete the user.' });
-  }
-};
+  };
+  
 
 exports.addFriend = async (req, res) => {
     try {
@@ -107,3 +140,32 @@ exports.addFriend = async (req, res) => {
       res.status(500).json({ error: 'Failed to add friend connection.' });
     }
   };
+
+  exports.removeFriend = async (req, res) => {
+    try {
+      const { userId, friendId } = req.params;
+  
+      // Remove friendId from the friends array of the user with userId
+      const user = await User.findByIdAndUpdate(
+        userId,
+        { $pull: { friends: friendId } },
+        { new: true }
+      );
+  
+      // Remove userId from the friends array of the user with friendId
+      const friend = await User.findByIdAndUpdate(
+        friendId,
+        { $pull: { friends: userId } },
+        { new: true }
+      );
+  
+      if (!user || !friend) {
+        return res.status(404).json({ message: 'User or friend not found.' });
+      }
+  
+      res.json({ message: 'Friend removed successfully.' });
+    } catch (err) {
+      res.status(500).json({ error: 'Failed to remove friend.' });
+    }
+  };
+  

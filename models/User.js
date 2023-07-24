8e91fdba08e300c8f05fadcfe68740fa9f66e7f0
retnodamayanti <mayahere5@gmail.com>
2023-07-24 14:00:46 +0930

    added pre save in User model

--- models/User.js
+++ models/User.js
@@ -1,36 +1,37 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      match: /^[\w-]+@[a-zA-Z\d]+(\.[a-zA-Z\d]+)*(\.[a-zA-Z]{2,})$/,
-    },
-    thoughts: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Thought',
-      },
-    ],
-    friends: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-      },
-    ],
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
   },
-  {
-    toJSON: { virtuals: true }, 
-  }
-);
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^[\w-]+@[a-zA-Z\d]+(\.[a-zA-Z\d]+)*(\.[a-zA-Z]{2,})$/,
+  },
+  thoughts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Thought', 
+    },
+  ],
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User', 
+    },
+  ],
+});
+
+// Ensure the 'friends' field is set to an empty array if it doesn't exist
+userSchema.pre('save', function (next) {
+  this.friends = this.friends || [];
+  next();
+});
 
 userSchema.virtual('friendCount').get(function () {
   return this.friends.length;
